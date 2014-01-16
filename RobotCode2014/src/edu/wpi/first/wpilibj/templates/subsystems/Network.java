fe259dd6828b82c06af04012efd1f988a6f00d00
sgoldman <sgoldman@IST-TECH-108-13.ccsd.d5>
2014-01-15 18:45:22 -0700

    Added Subsystem: Network Purpose: interface with the network tables and roborealm

---
+++ RobotCode2014/src/edu/wpi/first/wpilibj/templates/subsystems/Network.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author sgoldman
+ */
+public class Network extends Subsystem{
+
+    protected void initDefaultCommand() {
+        
+    }
+    
+}
