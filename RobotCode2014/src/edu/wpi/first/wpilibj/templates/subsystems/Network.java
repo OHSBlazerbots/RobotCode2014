082e97330cbd93a34308737ca50acf0377f2c2a2
jmuller4 <jmuller4@IST-TECH-108-14.ccsd.d5>
2014-01-16 17:09:31 -0700

    Have: 	Created a default constructor for Network which init a reference to SmartDashboard 	Filled the initDefaultCommand with a command 	COGArea too

--- RobotCode2014/src/edu/wpi/first/wpilibj/templates/subsystems/Network.java
+++ RobotCode2014/src/edu/wpi/first/wpilibj/templates/subsystems/Network.java
@@ -5,6 +5,9 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
+import edu.wpi.first.wpilibj.templates.commands.GetNetworkData;
 
 /**
  *
@@ -12,8 +15,32 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Network extends Subsystem{
 
-    protected void initDefaultCommand() {
-        
+    // Holds a reference to the SmartDashboard, where we get info from
+    private NetworkTable server;
+    
+    /**
+     * Creates the Network subsystem
+     * Initializes private reference to SmartDashboard
+     */
+    public Network() {
+        server = NetworkTable.getTable("SmartDashboard");
     }
     
+    /**
+     * Default method for 
+     */
+    
+    protected void initDefaultCommand() {
+        // Starts the robot with the following non-terminating command
+        setDefaultCommand(new GetNetworkData());
+    }
+    
+    public double getCOGArea() {
+        try {
+            return server.getNumber("COG_AREA");
+        }
+        catch (TableKeyNotDefinedException exp){
+            return 0;
+        }
+    }
 }
