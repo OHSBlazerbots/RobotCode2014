172012da546ee7fcdfdda149a7a8e1ccf367e879
sgoldman <sgoldman@IST-TECH-108-13.ccsd.d5>
2014-01-18 11:46:55 -0700

    Driving updates

--- RobotCode2014/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ RobotCode2014/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -12,7 +12,7 @@ public class RobotMap {
     // following variables to use with your drivetrain subsystem.
     public static final int FRONT_LEFT_MOTOR = 1;
     public static final int REAR_LEFT_MOTOR = 2;
-    public static final int FRONT_RIGHT_MOTOR = 3;
+    public static final int FRONT_RIGHT_MOTOR = 2;
     public static final int REAR_RIGHT_MOTOR = 4;
     
     // If you are using multiple modules, make sure to define both the port
